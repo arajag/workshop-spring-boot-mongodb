d854470a4893e7f1b72c4be82cee920c961be7d1
Andre Raja <andre.raja@gmail.com>
2020-05-02 20:32:21 +0100

    Connecting to MongoDB with repository and service.

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -2,9 +2,14 @@ package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user") // if we don't use the information inside parenthesis, the Framework will map the collection with the same class name in lowcase.
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private String id;
 	private String name;
 	private String email;
